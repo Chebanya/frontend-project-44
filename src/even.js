0bfa380d0c6878e4fbec25455fe2ce4aed5f03bb
Anna Karzhavina <Chebykina_7@mail.ru>
2024-01-18 00:21:15 +0500

    added Makefile bin/brain-even.js package.json src/even.js

---
+++ src/even.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name?: ');
+export const welcome = () => {
+    return `Hello, ${userName}!`;
+  };
+console.log(welcome())
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+for (let i = 0; i < 3; i++) {
+    const randomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }; 
+    let value = randomNumber(1, 100);
+    let randomNumberOutput = `Question: ${value}`;
+    console.log(randomNumberOutput);
+    const answeUser = readlineSync.question('Your answer: ');
+    let expectedResult = value % 2 === 0 ? 'yes' : 'no'
+    if (answeUser === expectedResult) {
+        console.log('Correct!') 
+        if (i === 2) {
+            console.log(`Congratulations, ${userName}!`);
+        }
+    } else {
+        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${userName}!`);
+        break
+    }
+}
+
+
+
+
+
+
+
