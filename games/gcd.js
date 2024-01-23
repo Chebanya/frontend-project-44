4e04f7ee7fd92f9a07c807b50c402836ac4d57b4
Anna Karzhavina <Chebykina_7@mail.ru>
2024-01-23 17:53:30 +0500

    added files bin/brain-gcd.js games/gcd.js

--- games/gcd.js
+++ games/gcd.js
@@ -2,15 +2,15 @@ import randomNumber from '../src/random.js';
 
 export const rule = 'Find the greatest common divisor of given numbers.';
 export const defineNode = () => {
-    const numberOne = randomNumber(1, 100);
-    const numberTwo = randomNumber(1, 100);
-    const randomExpression = `${numberOne}, ${numberTwo}`;
-    let expectedResult;
-    let smallestValue = numberOne <= numberTwo ? numberOne : numberTwo;
-      for (let i = 1; i <= smallestValue; i++) {
-        if (numberOne % i === 0 && numberTwo % i === 0) {
-            expectedResult = i;
-        }
+  const numberOne = randomNumber(1, 100);
+  const numberTwo = randomNumber(1, 100);
+  const randomExpression = `${numberOne}, ${numberTwo}`;
+  let expectedResult;
+  const smallestValue = numberOne <= numberTwo ? numberOne : numberTwo;
+  for (let i = 1; i <= smallestValue; i += 1) {
+    if (numberOne % i === 0 && numberTwo % i === 0) {
+      expectedResult = i;
     }
-      return [randomExpression, expectedResult.toString()];
+  }
+  return [randomExpression, expectedResult.toString()];
 };
